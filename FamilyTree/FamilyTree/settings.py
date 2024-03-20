7ad031e67218976d15eeed3093f8898254e4bf2b
WroAcc <polwroacc@gmail.com>
2024-03-20 12:18:45 +0100

    Person model

--- FamilyTree/FamilyTree/settings.py
+++ FamilyTree/FamilyTree/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -27,7 +26,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 INTERNAL_IPS = ['127.0.0.1']
-
 
 # Application definition
 
@@ -80,7 +78,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'FamilyTree.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
@@ -94,7 +91,6 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -114,7 +110,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
@@ -126,12 +121,11 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static',]
+STATICFILES_DIRS = [BASE_DIR / 'static', ]
 
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
--- FamilyTree/family_tree/views.py
+++ FamilyTree/family_tree/views.py
@@ -1,5 +1,8 @@
-from django.http import HttpResponse, HttpResponseNotFound
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseNotFound
 from django.shortcuts import render
+
+from .models import Person, FamilyRelationship
 
 
 def index(request):
@@ -19,4 +22,23 @@ def login(request):
 
 
 def page_not_found(request, exception):
-    return HttpResponseNotFound("<h1>Страница не найдена</h1>")
+    return HttpResponseNotFound("<h1>Page not found, OOOPS...</h1>")
+
+
+def person_list(request):
+
+    people = Person.objects.all()
+    return render(request, 'family_tree/person_list.html', {'people': people})
+# @login_required
+# def person_list(request):
+#     """ View all persons """
+#     user_family = request.person.family
+#     people = Person.objects.filter(family=user_family)
+#     return render(request, 'family_tree/person_list.html', {'people': people})
+
+
+def person_detail(request, pk):
+    """ Person information in details"""
+    person = Person.objects.get(pk=pk)
+    relationships = FamilyRelationship.objects.filter(from_person=person)
+    return render(request, 'family_tree/person_detail.html', {'person': person, 'relationships': relationships})
